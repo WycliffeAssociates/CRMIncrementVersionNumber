dd939eee20b36dda7b7030fc5ef87b21eab42e6c
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2017-12-04 11:07:55 -0500

    Initial application commit

---
+++ CRMIncrementVersionNumber/CommandLineArugments.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace CRMIncrementVersionNumber
+{
+    public class CommandLineArugments
+    {
+        [Option('c', "connection", HelpText = "CRM Connection String", Required = true)]
+        public string ConnectionString { get; set; }
+        [Option('s', "solution", HelpText = "Unqiue name of solution", Required = true)]
+        public string Solution { get; set; }
+        [Option('i', "increment", HelpText = "Version number to increment by", Required = true)]
+        public string VersionNumber { get; set; }
+
+        [HelpOption]
+        public string GetHelp()
+        {
+            return HelpText.AutoBuild(this, (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ CRMIncrementVersionNumber/Program.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMIncrementVersionNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandLineArugments arguments = new CommandLineArugments();
+            if (!CommandLine.Parser.Default.ParseArguments(args, arguments))
+            {
+                Environment.Exit(1);
+            }
+            CrmServiceClient service = new CrmServiceClient(arguments.ConnectionString);
+            if (!string.IsNullOrEmpty(service.LastCrmError))
+            {
+                Console.WriteLine("Error connecting to CRM " + service.LastCrmError);
+                Environment.Exit(2);
+            }
+            QueryExpression query = new QueryExpression("solution");
+            query.Criteria.AddCondition("uniquename", ConditionOperator.Equal, arguments.Solution);
+            query.ColumnSet = new ColumnSet("version");
+
+            var result = service.RetrieveMultiple(query);
+
+            if (result.Entities.Count == 0)
+            {
+                Console.WriteLine("Can't find a solution with the uniquename " + arguments.Solution);
+                Environment.Exit(3);
+            }
+
+            Entity solution = result.Entities[0];
+
+            List<int> currentVersion = ParseVersion((string)solution["version"]);
+            List<int> incrementVersion = ParseVersion(arguments.VersionNumber);
+
+            for (int i = 0; i < incrementVersion.Count; i++)
+            {
+                if (i >= currentVersion.Count)
+                {
+                    currentVersion.Add(incrementVersion[i] < 0 ? 0 : incrementVersion[i]);
+                }
+                else
+                {
+                    currentVersion[i] += incrementVersion[i];
+                }
+            }
+
+            string versionNumber = string.Join(".", currentVersion);
+
+            Console.WriteLine("Setting Version number to " + versionNumber);
+
+            solution["version"] = versionNumber;
+            service.Update(solution);
+        }
+        private static List<int> ParseVersion(string version)
+        {
+            List<int> output = new List<int>();
+            int tmp;
+            foreach (string s in version.Split('.'))
+            {
+                if (!int.TryParse(s, out tmp))
+                {
+                    throw new Exception("Invalid version number " + s);
+                }
+                output.Add(tmp);
+            }
+            return output;
+        }
+    }
+}
